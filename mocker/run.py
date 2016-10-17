00b881339b6cdb887297261505eda4c089f284ef
Anthony Shaw <anthonyshaw@apache.org>
2016-10-17 15:45:10 +1100

    extending run

--- mocker/run.py
+++ mocker/run.py
@@ -1,4 +1,6 @@
 import os
+import uuid
+import json
 
 from mocker import _base_dir_
 from .base import BaseDockerCommand
@@ -19,7 +21,13 @@ class RunCommand(BaseDockerCommand):
         image_name = kwargs['<name>']
 
         match = [i[3] for i in images if i[0] == image_name][0]
+
         target_file = os.path.join(_base_dir_, match)
+        with open(target_file) as tf:
+            image_details = json.loads(tf.read())
+
+        name = 'container_' + uuid.uuid1().replace('-', '_')
+        
         layer_dir = os.path.join(_base_dir_, match.replace('.json', ''), 'layers', 'contents')
 
         print(layer_dir)
