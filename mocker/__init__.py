b0823e01357a2dd3cc6582ebe7bd2d5eeab5fd98
Anthony Shaw <anthonyshaw@apache.org>
2016-10-18 13:14:19 +1100

    fancy logging

--- mocker/__init__.py
+++ mocker/__init__.py
@@ -1,4 +1,8 @@
 import os
+import colorama
+import logging
+import sys
+
 __copyright__ = """
    Copyright 2016 Anthony Shaw
 
@@ -35,6 +39,49 @@ _base_dir_ = os.path.join(os.path.expanduser('~'), 'mocker')
 if not os.path.exists(_base_dir_):
     os.makedirs(_base_dir_)
 
-import logging
-logging.basicConfig()
+
+# Courtesy http://plumberjack.blogspot.com/2010/12/colorizing-logging-output-in-terminals.html
+# Tweaked to use colorama for the coloring
+
+
+class ColorizingStreamHandler(logging.StreamHandler):
+    color_map = {
+        logging.DEBUG: colorama.Style.DIM + colorama.Fore.CYAN,
+        logging.WARNING: colorama.Fore.YELLOW,
+        logging.ERROR: colorama.Fore.RED,
+        logging.CRITICAL: colorama.Back.RED,
+    }
+
+    def __init__(self, stream, color_map=None):
+        logging.StreamHandler.__init__(self,
+                                       colorama.AnsiToWin32(stream).stream)
+        if color_map is not None:
+            self.color_map = color_map
+
+    @property
+    def is_tty(self):
+        isatty = getattr(self.stream, 'isatty', None)
+        return isatty and isatty()
+
+    def format(self, record):
+        message = logging.StreamHandler.format(self, record)
+        if self.is_tty:
+            # Don't colorize a traceback
+            parts = message.split('\n', 1)
+            parts[0] = self.colorize(parts[0], record)
+            message = '\n'.join(parts)
+        return message
+
+    def colorize(self, message, record):
+        try:
+            return (self.color_map[record.levelno] + message +
+                    colorama.Style.RESET_ALL)
+        except KeyError:
+            return message
+
 log = logging.getLogger()
+handler = ColorizingStreamHandler(sys.stdout)
+formatter = log.Formatter('%(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
